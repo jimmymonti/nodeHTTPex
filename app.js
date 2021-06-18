5a894da4ac54cc7acac3b7d4782bdb358e61666a
Jimmy Monti <email@example.com>
2021-06-18 15:21:59 -0700

    pushing to git hub

--- app.js
+++ app.js
@@ -1,8 +1,69 @@
-console.log("Hello World!\n==========\n");
+const http = reuire("http");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const populateHTML = (headingContent) => {
+    return `<main style="backgound-color: burlywood; height: 100vh; margin: 0; padding: 0; display: flex; justify-content: center; align-items; center;">
+    <h1>${headingContent}</h1>
+    </main>`;
+};
 
-const http = require("http");
+http
+.createServer((request, response) => {
+const { url, method } = request; 
+const chunks = [];
 
-// Finish setting up the server
+
+request.on("error", (error) => {
+    response.statusCode = 400;
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(error));
+    response.end();
+})
+}).on("data", (chunk) =>
+Chunks.push(chunk);
+}).on("end", () => {
+
+const body = Buffer.concat(chunks).toString();
+const responseBody = {
+    url,
+    method,
+    body,
+};
+response.on("error") => {
+    response.statusCode = 500;
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(error));
+    response.end();
+});
+
+    switch(url) {
+case "/":
+
+response.setHeader("Content-Type", "text/html");
+response.write(populateHTML("Raise the Anthem, Burlywood."));
+break;
+case "/about":
+
+const details = {
+    name: "Jimmy",
+    city: "Los Angeles"
+};
+response.setHeader("Content-Type", "application/json");
+response.write(JSON.stringify(details));
+
+Break:
+case "/echo":
+
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(responseBody));
+break:
+default;
+
+response.setHeader("Content-Type", "applicaion/json");
+response.write(populateHTML("404 Not Found. Try <a href=`http://localhost:3000`>this</a>"));
+)
+    );
+    }
+    return response.end();
+});
+})
+.listen(3000, () => console.log("Server listening on port 3000..."));
